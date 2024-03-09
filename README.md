9850e1d831f394915cf3b403a07283ebf4ceac9d
Hari K T <github@harikt.com>
2024-03-09 18:40:31 +0530

    Init experiments

---
+++ README.md
@@ -0,0 +1,17 @@
+# Learning Kotlin with Ktor
+
+Sample code to learn Kotlin with Ktor.
+
+## Plugin Experiments
+
+1. Api and Content Negotiation 
+1. Database connection with Exposed
+1. Websocket
+
+## Todo
+
+1. Serialization ( Create a user : `POST /users` ) . Fix exceptions thrown.
+1. Authentication
+1. Templating
+1. Testing
+1. Deployment
---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+val exposed_version: String by project
+val h2_version: String by project
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("io.ktor.plugin") version "2.3.9"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+}
+
+group = ""
+version = "0.0.1"
+
+application {
+    mainClass.set(".ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-websockets-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("io.ktor:ktor-server-freemarker-jvm")
+    implementation("io.ktor:ktor-server-openapi")
+    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-compression-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-status-pages-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
